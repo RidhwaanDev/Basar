9eab164a2e10b99997c05796af94675c9511f51e
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2021-01-04 20:57:45 -0500

    convert .txt to pdf finally

--- pdf.go
+++ pdf.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"github.com/signintech/gopdf"
+	"os/exec"
+	"fmt"
 )
 
 type pdf struct {
@@ -16,3 +17,25 @@ func (pdf p) GetPageCount(ch <-chan int) {}
 
 // mb size of PDF, usually when client uploads pdf, we will already know that
 func (pdf p) GetSize(ch <-chan int64) {}
+
+// headless chrome lol
+func ConvertTextToPDF {
+	// chrome --headless --disable-gpu --print-to-pdf input.txt
+	prg := "chrome"
+	arg1 := "--headless"
+	arg2 := "--disable-gpu"
+	arg3 := "--print-to-pdf"
+	val1 := "final_res.txt"
+	cmd := exec.Command(prg, arg1, arg2, arg3, val1)
+
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("an error in pdf convert" + "   " + stderr.String())
+		log.Fatal(err)
+	} else {
+		fmt.Println("successfully converted .txt to .pdf")
+	}
+}
