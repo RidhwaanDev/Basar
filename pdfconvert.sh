abefa9ab1655b70aea0195240b50d0be2f9f0893
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2021-01-05 14:14:51 -0500

    done

--- pdfconvert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "running chrome headless"
-
-alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-
-chrome --headless --disable-gpu --print-to-pdf input.txt
-ls
-
