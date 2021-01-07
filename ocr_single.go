0ed6819be39f8016ad99c3ffc0a0a47a84d5ff34
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2021-01-06 19:19:12 -0500

    ocr_single update iwth mutex

--- ocr_single.go
+++ ocr_single.go
@@ -6,24 +6,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"sync"
 )
+
+// sync file i/o
+var mutex = &sync.Mutex{}
 
 func main() {
 	if len(os.Args) <= 1 {
-		fmt.Println("need full file name")
+		fmt.Println("please provide a file or directory name")
 		return
 	}
 
-	ocr(os.Args[1])
+	name := os.Args[1]
+	fi, err := os.Stat(name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	switch mode := fi.Mode(); {
+	case mode.IsDir():
+		ocr_dir(name)
+	case mode.IsRegular():
+		// single file
+		//		detectText(name)
+	}
 }
 
 func ocr_dir(file string) {
-
-	if len(os.Args) <= 1 {
-		fmt.Println("send location")
-		return
-	}
-	path := "shaa"
+	path := "images"
 
 	dir, err := os.Open(path)
 
@@ -31,15 +43,23 @@ func ocr_dir(file string) {
 		log.Fatalf("Error opening directory: %s", err)
 	}
 
+	f, err := os.Create("output.txt")
+	defer f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	defer dir.Close()
 	list, _ := dir.Readdirnames(0)
 	for _, name := range list {
 		var filePath = path + "/" + name
-		ocr(filePath)
+		go detectText(filePath, f)
 	}
 }
 
-func ocr(file string) {
+// writes to output.txt
+func detectText(file string, f *os.File) {
 	fmt.Printf("detecting text in %s\n", file)
 	ctx := context.Background()
 
@@ -47,13 +67,6 @@ func ocr(file string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	f, err := os.Open(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer f.Close()
 
 	image, err := vision.NewImageFromReader(f)
 	if err != nil {
@@ -65,16 +78,32 @@ func ocr(file string) {
 		log.Fatal(err)
 	}
 
-	fmt.Println("No text found.")
+	outputString := make([]string, 50)
+
 	if len(annotations) == 0 {
 		fmt.Println("No text found.")
 	} else {
 		// fmt.Fprintln(w, "Text:")
 		for _, annotation := range annotations {
 			// the first line is the ocr of the entire document
-			fmt.Println(annotation.Description)
+			outputString = append(outputString, annotation.Description)
+			// fmt.Println(annotation.Description)
 			break
 			// fmt.Fprintf(w, "%q\n", annotation.Description)
 		}
+	}
+
+	output := strings.Join(outputString, " ")
+
+	// sync file i/o
+	mutex.Lock()
+	// write the OCR results to the file
+	if _, err := f.WriteString(output); err != nil {
+		log.Println(err)
+	}
+	mutex.Unlock()
+
+	if err != nil {
+		fmt.Println(err)
 	}
 }
