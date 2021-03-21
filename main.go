452e5e680b6135d92aafb5130ffa09d8c0c79a8d
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2021-03-21 15:03:12 -0400

    upload file, do OCR, and download res to disk is wokring

--- main.go
+++ main.go
@@ -5,36 +5,92 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"hash/fnv"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"cloud.google.com/go/storage"
 	vision "cloud.google.com/go/vision/apiv1"
+	"google.golang.org/api/iterator"
 	visionpb "google.golang.org/genproto/googleapis/cloud/vision/v1"
 )
 
 var buf bytes.Buffer
 
+func hash(s string) string {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	id := h.Sum32()
+	return strconv.FormatUint(uint64(id), 10)
+}
+
+const BUCKET_NAME = "basar-ocr-pdf-storage"
+
+func getNamesOfOCRResult(prefix string) []string {
+	query := &storage.Query{Prefix: prefix}
+
+	var names []string
+
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	bkt := client.Bucket(BUCKET_NAME)
+	it := bkt.Objects(ctx, query)
+
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		names = append(names, attrs.Name)
+	}
+
+	for _, name := range names {
+		fmt.Println(name)
+	}
+	return names
+}
+
 func main() {
 	if len(os.Args) <= 1 {
 		log.Fatal("please support filename")
 	}
-	fileName := os.Args[1]
 
-	if err := UploadFile(&buf, fileName); err != nil {
+	fileName := os.Args[1]
+	fileNameId := hash(fileName)
+	// fmt.Printf("file name id hash %s\n", fileNameId)
+
+	// upload the file to do OCR on it
+	if err := UploadFile(&buf, fileNameId, fileName); err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Println("success uploading file")
 	}
 
-	src := fmt.Sprintf("gs://basar-ocr-pdf-storage/%s", fileName)
-	// dst := fmt.Sprintf("gs://basar-ocr-pdf-storage/%s", fileName)
-	err := DetectAsyncDocumentURI(&buf, src, "gs://basar-ocr-pdf-storage/OCR-Result")
+	// file to do OCR on
+	src := fmt.Sprintf("gs://basar-ocr-pdf-storage/%s", fileNameId)
+	des := fmt.Sprintf("gs://basar-ocr-pdf-storage/%s%s", fileNameId, "-Result")
+
+	// detect OCR in the file we just uploaded in the OCR-Result directory
+	err := DetectAsyncDocumentURI(&buf, src, des)
 	if err != nil {
 		fmt.Printf("Error in OCR: %s", err)
+	}
+
+	fileNames := getNamesOfOCRResult(fileNameId)
+	for _, item := range fileNames {
+		downloadFile(&buf, item)
 	}
 
 	// bytes, err := downloadFile(&buf)
@@ -100,9 +156,9 @@ func DetectAsyncDocumentURI(w io.Writer, gcsSourceURI, gcsDestinationURI string)
 }
 
 // uploadFile uploads an object.
-func UploadFile(w io.Writer, fname string) error {
+func UploadFile(w io.Writer, fileNameHash string, localfileName string) error {
 	bucket := "basar-ocr-pdf-storage"
-	object := fname
+	object := fileNameHash
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
@@ -111,7 +167,7 @@ func UploadFile(w io.Writer, fname string) error {
 	defer client.Close()
 
 	// Open local file.
-	f, err := os.Open(fname)
+	f, err := os.Open(localfileName)
 	if err != nil {
 		return fmt.Errorf("os.Open: %v", err)
 	}
@@ -132,14 +188,44 @@ func UploadFile(w io.Writer, fname string) error {
 	return nil
 }
 
+// func downloadOCRResults() ([]byte, error) {
+// 	itemsToDownload := getNamesOfOCRResult()
+// 	ctx := context.Background()
+// 	client, err := storage.NewClient(ctx)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("storage.NewClient: %v", err)
+// 	}
+// 	defer client.Close()
+//
+// 	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+// 	defer cancel()
+// 	for _, object := range itemsToDownload {
+// 		rc, err := client.Bucket(BUCKET_NAME).Object(object).NewReader(ctx)
+// 		if err != nil {
+// 			return nil, fmt.Errorf("Object(%q).NewReader: %v", object, err)
+// 		}
+// 		defer rc.Close()
+//
+// 		data, err := ioutil.ReadAll(rc)
+// 		if err != nil {
+// 			return nil, fmt.Errorf("ioutil.ReadAll: %v", err)
+// 		}
+// 		fmt.Fprintf(w, "Blob %v downloaded.\n", object)
+// 		err := ioutil.WriteFile(object, data, 666)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 	}
+// 	return nil
+// }
+//
 // downloadFile downloads an object.
-func downloadFile(w io.Writer) ([]byte, error) {
+func downloadFile(w io.Writer, object string) {
 	bucket := "basar-ocr-pdf-storage"
-	object := "singlepage.pdf"
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("storage.NewClient: %v", err)
+		fmt.Printf("storage.NewClient: %v", err)
 	}
 	defer client.Close()
 
@@ -148,14 +234,21 @@ func downloadFile(w io.Writer) ([]byte, error) {
 
 	rc, err := client.Bucket(bucket).Object(object).NewReader(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("Object(%q).NewReader: %v", object, err)
+		fmt.Printf("Object(%q).NewReader: %v", object, err)
 	}
 	defer rc.Close()
 
 	data, err := ioutil.ReadAll(rc)
+
 	if err != nil {
-		return nil, fmt.Errorf("ioutil.ReadAll: %v", err)
+		fmt.Printf("ioutil.ReadAll: %v", err)
 	}
 	fmt.Fprintf(w, "Blob %v downloaded.\n", object)
-	return data, nil
+
+	// write file to disk
+	err = ioutil.WriteFile(object, data, 0644)
+	if err != nil {
+		fmt.Printf("ioutil.WriteFile: %v", err)
+	}
+
 }
